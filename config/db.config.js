a68603fb300960751b187d472d2d4810763a20f3
kelvin <jkakampa@gmail.com>
2023-07-22 12:31:02 +0300

    assignment

---
+++ config/db.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    HOST: "inpro7.fcomet.com",
+    USER: "schoolpe_group1_user",
+    PASSWORD: "9aFkIR5m3CtU#2wK",
+    DB: "schoolpe_group1",
+    dialect: "mysql",
+    pool: {
+        // maximum number of connections to db pool
+        max: 5,
+        // minimum number of connections to db pool 
+        min: 0,
+        // maximum time, in milliseconds, that the db pool tries to connect before it throws an error
+        acquire: 30000,
+        // maximum time, in milliseconds, that a connection can be idle before being released
+        idle: 1000
+
+    }
+}
